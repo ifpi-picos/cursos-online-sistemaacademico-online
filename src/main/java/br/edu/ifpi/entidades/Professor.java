4fda938bf5d98ec939b72ccccb0203b4a4270237
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-11-07 14:53:28 -0300

    classe professor

--- src/main/java/br/edu/ifpi/entidades/Professor.java
+++ src/main/java/br/edu/ifpi/entidades/Professor.java
@@ -1,5 +1,36 @@
 package br.edu.ifpi.entidades;
 
 public class Professor {
-    
+    private String nome;
+    private long id;
+    private String email;
+    private Curso curso;
+
+    public Professor(String nome, long id, String email, Curso curso) {
+        this.nome = nome;
+        this.id = id;
+        this.email = email;
+        this.curso = curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+
 }
