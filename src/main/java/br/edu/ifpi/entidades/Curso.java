05f4cf9d23957a164d490c7d90da98a0e07e9e37
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-11-07 14:42:40 -0300

    organizando

--- src/main/java/br/edu/ifpi/entidades/Curso.java
+++ src/main/java/br/edu/ifpi/entidades/Curso.java
@@ -3,35 +3,30 @@ package br.edu.ifpi.entidades;
 import br.edu.ifpi.enums.StatusCurso;
 
 public class Curso {
-   
+
     private String nome;
-    
-
     private StatusCurso status;
-    
     private int cargahoraria;
-   
 
+    public Curso(String nome, int cargahoraria) {
 
-public Curso(String nome, int cargahoraria) {
-      
         this.nome = nome;
         this.cargahoraria = cargahoraria;
 
     }
 
-
-public Curso(){
+    public Curso() {
 
     }
-
 
     public String getNome() {
         return nome;
     }
+
     public StatusCurso getStatus() {
         return status;
     }
+
     public int getCargahoraria() {
         return cargahoraria;
     }
