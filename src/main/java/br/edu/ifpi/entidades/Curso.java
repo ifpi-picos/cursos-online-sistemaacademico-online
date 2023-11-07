f292323808ee7809d74c1e291508a97b69dd494a
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-11-07 14:35:25 -0300

    criação de pasta entidades

---
+++ src/main/java/br/edu/ifpi/entidades/Curso.java
@@ -0,0 +1,15 @@
+package br.edu.ifpi.entidades;
+
+public class Curso {
+   
+    private String nome;
+    // private Status status;
+    private int cargahoraria;
+   
+public Curso(String nome, int cargahoraria) {
+      
+        this.nome = nome;
+        this.cargahoraria = cargahoraria;
+    }
+
+}
