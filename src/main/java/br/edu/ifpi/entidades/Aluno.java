65514b59465097f2afbe8dbc38bb19d36494bf90
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-11-07 14:51:26 -0300

    classe aluno

--- src/main/java/br/edu/ifpi/entidades/Aluno.java
+++ src/main/java/br/edu/ifpi/entidades/Aluno.java
@@ -1,5 +1,36 @@
 package br.edu.ifpi.entidades;
 
 public class Aluno {
-    
+
+    private String nome;
+    private long id;
+    private String email;
+    private Curso curso;
+
+    public Aluno(String nome, int id, String email, Curso curso) {
+        this.nome = nome;
+        this.id = id;
+        this.email = email;
+        this.curso = curso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
 }
