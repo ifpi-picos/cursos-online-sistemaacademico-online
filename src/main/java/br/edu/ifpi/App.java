975d4f80828ae3c5844448c576f9e48b0cf8533f
kednaleal <capic.2023118tads0017@aluno.ifpi.edu.br>
2023-11-22 17:31:45 -0300

    feat: cadastrei dois alunos na tabela

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -9,9 +9,11 @@ import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
+import br.edu.ifpi.DAO.AlunoDao;
 import br.edu.ifpi.DAO.Conexacao;
 import br.edu.ifpi.DAO.ProfessorDao;
 import br.edu.ifpi.DAO.CursoDao;
+import br.edu.ifpi.entidades.Aluno;
 import br.edu.ifpi.entidades.Curso;
 import br.edu.ifpi.entidades.Professor;
 import br.edu.ifpi.enums.StatusCurso;
@@ -27,6 +29,7 @@ public class App {
 
         ProfessorDao ProfessorDao = new ProfessorDao(conexao);
         CursoDao cursoDao = new CursoDao(conexao);
+        AlunoDao alunoConexao = new AlunoDao(conexao);
 
         // Professor novoProfessor1 = new Professor("zé", "789@prof.com");
         // Professor novoProfessor2 = new Professor("João", "joao@prof.com");
@@ -35,34 +38,33 @@ public class App {
         // ProfessorDao.cadastrar(novoProfessor1);
         // ProfessorDao.cadastrar(novoProfessor2);
         // ProfessorDao.cadastrar(novoProfessor3);
+        // ProfessorDao.consultarTodos();
 
-        // Curso curso = new Curso(4,"Algoritmo", 100, 1, StatusCurso.ABERTO);
+        Curso curso = new Curso(4, "Algoritmo", 100, 1, StatusCurso.ABERTO);
         // cursoDao.cadastrar(curso);
         cursoDao.consultarTodos();
         cursoDao.visualizarCursos();
-        
-
-        // ProfessorDao.consultarTodos();
 
         CursoDao dao = new CursoDao(conexao);
-        // Curso novoCurso = new Curso(4, "Poo", 60, 3, StatusCurso.ABERTO);
+        Curso novoCurso = new Curso(4, "Poo", 60, 3, StatusCurso.ABERTO);
         // dao.cadastrar(novoCurso);
         List<Curso> cursos = dao.consultarTodos();
-        
-        
-        for (Curso curso : cursos){
+
+        for (Curso c : cursos) {
             System.out.println("id " + curso.getId());
             System.out.println("nome " + curso.getNome());
         }
 
+        Aluno aluno1 = new Aluno("samya", "sam@aluno.ifpi.edu.br");
+        // alunoConexao.cadastrar(aluno1);
+        Aluno al2 = new Aluno("Katarina", "katarina@aluno.ifpi.edu.br");
+        alunoConexao.cadastrar(al2);
+
         // int i = 0;
         // while (i <= profs.size()) {
-        //     System.out.println("Nome: " + profs.get(i).getNome());
-        //     i++;
+        // System.out.println("Nome: " + profs.get(i).getNome());
+        // i++;
         // }
-        
-        // System.out.println("TESTANDO CONFLITOS");
-        // System.out.println("TESTANDO CONFLITOS");
-        
+
     }
 }
