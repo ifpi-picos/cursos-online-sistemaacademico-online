fb3e7995413f5dee8e3cdc1c3bf35ff35531c4cb
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2023-11-24 14:48:24 -0300

    comentarios professor

--- src/test/java/br/edu/ProfessorTest.java
+++ src/test/java/br/edu/ProfessorTest.java
@@ -10,30 +10,39 @@ import br.edu.ifpi.entidades.Professor;
 
 public class ProfessorTest {
         public static void main(String[] args) {
+                //conexão com banco de dados
                 Connection conexao = Conexacao.getConexao();
-
+               
                 ProfessorDao ProfessorDao = new ProfessorDao(conexao);
+                //consultar toda a tabela(id,professor,email)
+                
                 ProfessorDao.consultarTodos();
+                //vizualizar tabela(professor e curso)
                 ProfessorDao.visualizarProfessor();
                 
-                
+               
                 Scanner scan = new Scanner(System.in);
-                System.out.println(" Digite o id do professor que 7deseja alterar: ");
+
+                //entrada de dados
+                System.out.println(" Digite o id do professor que deseja alterar: ");
                 int Id_alter = scan.nextInt();
                 scan.nextLine();
 
                 Professor prof_alter = new Professor();
 
+                //pelo id alterar nome e email
                 prof_alter.setId(Id_alter);
+
                 System.out.println("Novo Nome: ");
                 prof_alter.setNomeP(scan.nextLine());
 
                 System.out.println("Novo Email: ");
                 prof_alter.setEmail(scan.next());
 
-                
+                //alterar professor 
                 ProfessorDao.alterar(prof_alter);
 
+                //tabela já atualizada
                 ProfessorDao.consultarTodos();
         }
 }
