1ab1ca9b233e739d66687d3ce3c1c06bc68e8a14
AlunaEmileny <capic.2023118tads0008@aluno.ifpi.edu.br>
2023-12-01 20:49:28 -0300

    complemento menu

--- src/main/java/br/edu/ifpi/App.java
+++ src/main/java/br/edu/ifpi/App.java
@@ -6,18 +6,24 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
+import br.edu.ifpi.DAO.AlunoDao;
 import br.edu.ifpi.DAO.AutenticacaoDao;
 import br.edu.ifpi.DAO.Conexacao;
 import br.edu.ifpi.DAO.ProfessorDao;
+import br.edu.ifpi.entidades.Aluno;
 import br.edu.ifpi.entidades.Curso;
 import br.edu.ifpi.entidades.Professor;
 import br.edu.ifpi.enums.StatusCurso;
 
+
+
 public class App {
     public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "true");
         Connection conexao = Conexacao.getConexao();
         AutenticacaoDao autentificacaodao = new AutenticacaoDao(conexao);
         ProfessorDao ProfessorDao = new ProfessorDao(conexao);
+        AlunoDao alunoDao = new AlunoDao(conexao);
         String[] opcoes = { "professor", "aluno" };
         int escolha = JOptionPane.showOptionDialog(
                 null,
@@ -140,6 +146,77 @@ public class App {
 
         } else if (escolha == 1) {
             JOptionPane.showMessageDialog(null, "Você escolheu aluno ");
+            String input = JOptionPane.showInputDialog("Digite seu id :");
+            int matriculaId = Integer.parseInt(input);
+            autentificacaodao.autenticarAluno(matriculaId);
+
+            String entrada = JOptionPane.showInputDialog(null,
+                    "1º Matricular;\n2ª consultar \n3º alterar \n4ª cancelar matricula \n5ª Gerar estatisticas \n6 ª visualizar cursos matriculados \n7ª sair \n DIGITE UMA OPÇÃO:",
+                    " Menu", JOptionPane.QUESTION_MESSAGE);
+
+            int opcao = Integer.parseInt(entrada);
+
+            switch (opcao) {
+                case 1:
+                    String nomeA = JOptionPane.showInputDialog("Digite seu nome:");
+                    String emailA = JOptionPane.showInputDialog("Digite um seu email :");
+
+                    Aluno al = new Aluno(nomeA, emailA);
+                    alunoDao.cadastrar(al);
+                    JOptionPane.showMessageDialog(null, "O professor foi cadastrado com sucesso! ");
+                    
+                    break;
+                case 2: 
+                    System.out.println("-----------------------------------------------------");
+                    alunoDao.consultarTodos();
+                    System.out.println("-----------------------------------------------------");
+                break;
+                case 3:
+                    System.out.println("-----------------------------------------------------");
+                    alunoDao.consultarTodos();
+                    System.out.println("-----------------------------------------------------");
+
+                    String id_alter = JOptionPane.showInputDialog("Digite o seu id para alterar: ");
+                    int alter_id = Integer.parseInt(id_alter);
+
+                    String nome_alter = JOptionPane.showInputDialog("Digite seu nome:");
+                    String email_alter = JOptionPane.showInputDialog("Digite um seu email :");
+                    Aluno aluno_alter = new Aluno(nome_alter, email_alter);
+                    alunoDao.alterar(aluno_alter);
+                    System.out.println("alterações realizadas");
+                break;
+                case 4:
+                       System.out.println("-----------------------------------------------------");
+                        alunoDao.consultarTodos();
+                        System.out.println("-----------------------------------------------------");
+
+                        String id_remov = JOptionPane
+                                .showInputDialog("Digite o id do aluno que vc desenja remover : ");
+                        int remov_id = Integer.parseInt(id_remov);
+
+                        Aluno aluno_remove = new Aluno();
+                        alunoDao.remover(aluno_remove);
+
+                        System.out.println("-----------------------------------------------------");
+                        ProfessorDao.consultarTodos();
+                        System.out.println("-----------------------------------------------------");
+                break;
+                case 5: 
+                  alunoDao.criarAproveitamentoCurso();
+                break;
+                case 6 :
+                 String varMsm = JOptionPane
+                                .showInputDialog("Digite sua matricula : ");
+                        int idExibicao = Integer.parseInt(varMsm);
+
+                  alunoDao.exibirCursosMatriculados(idExibicao);
+                break;
+                case 7:
+                aux = false;
+                break;
+                default:
+                    break;
+            }
 
         } else {
             JOptionPane.showMessageDialog(null, "Nenhuma opção selecionada");
